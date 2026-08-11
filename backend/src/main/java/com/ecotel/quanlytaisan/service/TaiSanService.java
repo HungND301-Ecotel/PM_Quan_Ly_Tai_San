d6d97a23195f7c9e846c20ae0d65043a9d767aaa
ecotelDev <dev@ecotel.com.vn>
2026-08-11 11:03:50 +0700

    Dev/uongbi (#61)
    * fix lỗi trinh duyệt, điều chuyển tài sản
    
    * fix cập nhật tài sản phát sinh

--- backend/src/main/java/com/ecotel/quanlytaisan/service/TaiSanService.java
+++ backend/src/main/java/com/ecotel/quanlytaisan/service/TaiSanService.java
@@ -1364,7 +1364,6 @@ public class TaiSanService {
                 .map(TaiSanDTO::getId)
                 .collect(Collectors.toList());
         System.out.println("2");
-        System.out.println("ids: " + ids);
         // Fetch all related data in parallel
 
         CompletableFuture<List<TaiSanDTO>> taiSanConFuture = CompletableFuture.supplyAsync(() -> 
